b51d62ebbdec1ffc44fd5c8dab8dbbda49590ced
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2023-03-17 19:19:16 +0300

    pr11: rev 1-3

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -80,22 +80,6 @@ class Api {
       headers: this._headers,
     }).then((res) => this._result(res));
   }
-
-  //   //добавление лайка карточке
-  //   addLike(idCard) {
-  //     return fetch(`${this._url}/cards/likes/${idCard}`, {
-  //       method: "PUT",
-  //       headers: this._headers,
-  //     }).then((res) => this._result(res));
-  //   }
-
-  //   // слимаем лайк карточке
-  //   removeLike(idCard) {
-  //     return fetch(`${this._url}/cards/likes/${idCard}`, {
-  //       method: "DELETE",
-  //       headers: this._headers,
-  //     }).then((res) => this._result(res));
-  //   }
 }
 const api = new Api({
   url: "https://mesto.nomoreparties.co/v1/cohort-58",
